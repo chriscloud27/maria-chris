68871aacbc998315e823c59a1c67e97e6e6f8631
Christian Weber <chris@fairup.dev>
2025-08-09 19:37:01 +0200

    fix(error): hydration with double loaded fonts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,47 +1,18 @@
-import { Playfair_Display, Great_Vibes, Lato } from "next/font/google";
-import {ReactNode} from 'react';
-import {NextIntlClientProvider, useMessages} from 'next-intl';
+import { ReactNode } from 'react';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
 import WhatsAppButton from "@/components/WhatsAppButton";
-
-import "@fontsource/playfair-display";
-import "@fontsource/great-vibes";
-import "@fontsource/lato";
-
-const playfairDisplay = Playfair_Display({
-  subsets: ["latin"],
-  variable: "--font-playfair-display",
-});
-
-const greatVibes = Great_Vibes({
-  subsets: ["latin"],
-  weight: "400",
-  variable: "--font-great-vibes",
-});
-
-const lato = Lato({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-lato",
-});
 
 type Props = {
   children: ReactNode;
-  params: {locale: string};
+  params: { locale: string };
 };
 
-export default function LocaleLayout({children, params: {locale}}: Props) {
-  // Providing all messages to the client
-  // side is the easiest way to get started
+export default function LocaleLayout({ children, params: { locale } }: Props) {
   const messages = useMessages();
- 
   return (
-    <html lang={locale}>
-      <body className={`${playfairDisplay.variable} ${greatVibes.variable} ${lato.variable} font-sans antialiased`}>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          {children}
-          <WhatsAppButton />
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      {children}
+      <WhatsAppButton />
+    </NextIntlClientProvider>
   );
 }
