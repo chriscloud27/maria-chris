1ee2138ea6fe944bb22445a4ab8ec55061850b03
Chris <42301973+chriscloud27@users.noreply.github.com>
2026-07-26 08:34:11 +0200

    fix(npm): runtime issues fixed

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,9 @@
-import {createSharedPathnamesNavigation} from 'next-intl/navigation';
- 
+import {createNavigation} from 'next-intl/navigation';
+
 export const locales = ['en', 'de', 'es'] as const;
 export const localePrefix = 'as-needed';
- 
+
+export const defaultLocale = 'en' as const;
+
 export const {Link, redirect, usePathname, useRouter} =
-  createSharedPathnamesNavigation({locales, localePrefix});
+  createNavigation({locales, localePrefix, defaultLocale});
